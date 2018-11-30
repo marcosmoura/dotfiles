7675b537730b15dd96823590b21b501918850027
Marcos <marcosvmmoura@gmail.com>
2018-11-30 15:49:12 -0200

    add missing development dependencies

--- scripts/development.sh
+++ scripts/development.sh
@@ -8,6 +8,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+pod setup
 
 printMsg "DEVELOPMENT" "Installing Yarn Packages..."
 yarn global add @vue/cli
