1ebcd48e50ca58c72d753a71770c58f0670e60b0
Marcos <marcosvmmoura@gmail.com>
2018-12-14 11:48:52 -0200

    improve layout of new apps

--- dots/.hammerspoon/dotfiles/layout.lua
+++ dots/.hammerspoon/dotfiles/layout.lua
@@ -27,8 +27,12 @@ end
 function setupManualWindows (windowLayout)
   setupTilingWindow(windowLayout, 'Franz', 320, 127, 1280, 800)
   setupTilingWindow(windowLayout, 'Finder', 16, 38, 848, 500)
-  setupTilingWindow(windowLayout, 'Wake Up Time', 156, 675, 848, 526)
-  setupTilingWindow(windowLayout, 'Calendar', 880, 38, 1024, 980)
+  setupTilingWindow(windowLayout, 'Wake Up Time', 156, 675, 848, 524)
+  setupTilingWindow(windowLayout, 'Calendar', 880, 38, 1024, 978)
+  setupTilingWindow(windowLayout, 'Paymo Widget', 16, 38, 600, 978)
+  setupTilingWindow(windowLayout, 'Notion', 632, 38, 1272, 978)
+  setupTilingWindow(windowLayout, 'Agenda', 632, 38, 1272, 978)
+  setupTilingWindow(windowLayout, 'Things', 632, 38, 1272, 978)
 end
 
 return function ()
