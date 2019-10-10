079411da4ac45183c4e749fd8aeddb953ae77c33
Marcos Moura <marcosvmmoura@gmail.com>
2019-10-10 11:36:47 +0200

    add Ethernet Status

--- scripts/cask.sh
+++ scripts/cask.sh
@@ -75,9 +75,10 @@ printSuccess "BREW" "Installed with success..."
 
 printMsg "MAC APPS" "Installing App Store Apps..."
 
-mas install 948176063 # Boom 2
-mas install 915542151 # Monity
-mas install 495945638 # Wake Up Time
-mas install 497799835 # Xcode
+mas install 948176063  # Boom 2
+mas install 915542151  # Monity
+mas install 495945638  # Wake Up Time
+mas install 1186187538 # Ethernet Status
+mas install 497799835  # Xcode
 
 printSuccess "MAC APPS" "Installed with success..."
