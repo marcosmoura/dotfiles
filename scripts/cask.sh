c80c1315803072abf37afac791192ed084090b16
Marcos Moura <marcosvmmoura@gmail.com>
2021-03-25 01:57:23 +0100

    update brew packages and casks

--- scripts/cask.sh
+++ scripts/cask.sh
@@ -4,73 +4,72 @@ reset
 printMsg "BREW" "Installing casks..."
 
 # Browsers
-brew cask install --no-quarantine firefox-nightly
-brew cask install --no-quarantine google-chrome-canary
+brew install --no-quarantine firefox-nightly
+brew install --no-quarantine google-chrome-canary
 
 # Development
-brew cask install --no-quarantine figma
-brew cask install --no-quarantine hyper
-brew cask install --no-quarantine imageoptim
-brew cask install --no-quarantine iterm2
-brew cask install --no-quarantine visual-studio-code-insiders
-brew cask install --no-quarantine zeplin
-
-# Communication
-brew cask install --no-quarantine discord
-brew cask install --no-quarantine whatsapp
-
-# Cloud
-brew cask install --no-quarantine google-backup-and-sync
-
-# Games
-brew cask install --no-quarantine steam
-
-# Productivity
-brew cask install --no-quarantine alfred
-brew cask install --no-quarantine hammerspoon
-brew cask install --no-quarantine hiddenbar
-brew cask install --no-quarantine kap
-brew cask install --no-quarantine koekeishiya/formulae/yabai
-brew cask install --no-quarantine muzzle
-brew cask install --no-quarantine numi
-
-# Media
-brew cask install --no-quarantine iina
-brew cask install --no-quarantine spotify
-brew cask install --no-quarantine subtitle-master
-brew cask install --no-quarantine transmission
-brew cask install --no-quarantine vlc
-
-# Quicklook extensions
-brew cask install --no-quarantine qlcolorcode
-brew cask install --no-quarantine qlimagesize
-brew cask install --no-quarantine qlmarkdown
-brew cask install --no-quarantine qlstephen
-brew cask install --no-quarantine qlvideo
-brew cask install --no-quarantine quicklook-json
-brew cask install --no-quarantine quicklookase
-brew cask install --no-quarantine suspicious-package
-brew cask install --no-quarantine webpquicklook
+brew install --no-quarantine figma
+brew install --no-quarantine imageoptim
+brew install --no-quarantine inkscape
+brew install --no-quarantine iterm2
+brew install --no-quarantine sf-symbols
+brew install --no-quarantine visual-studio-code-insiders
+brew install --no-quarantine zeplin
 
 # Fonts
-brew cask install --no-quarantine font-cascadia-mono
-brew cask install --no-quarantine font-fira-code
-brew cask install --no-quarantine font-hack
-brew cask install --no-quarantine font-hack-nerd-font
-brew cask install --no-quarantine font-jetbrains-mono
+brew install --no-quarantine font-cascadia-mono
+brew install --no-quarantine font-fira-code
+brew install --no-quarantine font-hack-nerd-font
+brew install --no-quarantine font-jetbrains-mono
+
+# Communication
+brew install --no-quarantine discord
+brew install --no-quarantine whatsapp
+
+# Cloud
+brew install --no-quarantine google-backup-and-sync
+
+# Games
+brew install --no-quarantine steam
+
+# Productivity
+brew install --no-quarantine alfred
+brew install --no-quarantine hammerspoon
+brew install --no-quarantine hiddenbar
+brew install --no-quarantine kap
+brew install --no-quarantine koekeishiya/formulae/yabai
+brew install --no-quarantine muzzle
+brew install --no-quarantine numi
+
+# Media
+brew install --no-quarantine audio-hijack
+brew install --no-quarantine iina
+brew install --no-quarantine spotify
+brew install --no-quarantine subtitle-master
+brew install --no-quarantine transmission
+brew install --no-quarantine vlc
+
+# Quicklook extensions
+brew install --no-quarantine qlcolorcode
+brew install --no-quarantine qlimagesize
+brew install --no-quarantine qlmarkdown
+brew install --no-quarantine qlstephen
+brew install --no-quarantine qlvideo
+brew install --no-quarantine quicklook-json
+brew install --no-quarantine quicklookase
+brew install --no-quarantine webpquicklook
 
 # Other
-brew cask install --no-quarantine adguard
-brew cask install --no-quarantine android-file-transfer
-brew cask install --no-quarantine appcleaner
-brew cask install --no-quarantine captin
-brew cask install --no-quarantine caption
-brew cask install --no-quarantine lyricsx
-brew cask install --no-quarantine mullvadvpn
-brew cask install --no-quarantine notion
-brew cask install --no-quarantine opencore-configurator
-brew cask install --no-quarantine shifty
-brew cask install --no-quarantine the-unarchiver
+brew install --no-quarantine adguard
+brew install --no-quarantine appcleaner
+brew install --no-quarantine captin
+brew install --no-quarantine caption
+brew install --no-quarantine lyricsx
+brew install --no-quarantine mullvadvpn
+brew install --no-quarantine notion
+brew install --no-quarantine opencore-configurator
+brew install --no-quarantine shifty
+brew install --no-quarantine the-unarchiver
 
 printSuccess "BREW" "Installed with success..."
 
@@ -79,6 +78,5 @@ printMsg "MAC APPS" "Installing App Store Apps..."
 mas install 1319778037 # iStats Menu
 mas install 497799835  # Xcode
 mas install 948176063  # Boom 2
-mas install 980888073  # Crypto Pro
 
 printSuccess "MAC APPS" "Installed with success..."
