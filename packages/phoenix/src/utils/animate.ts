3c6814f1c5c191775d3a3ccd8906fc069e423b50
Marcos Moura <marcosvmmoura@gmail.com>
2022-07-26 14:34:38 +0200

    disable phoenix window animations (#8)

--- packages/phoenix/src/utils/animate.ts
+++ packages/phoenix/src/utils/animate.ts
@@ -1,6 +1,6 @@
 import { animate, cubicBezier, KeyframeOptions } from 'popmotion'
 
-import { animationDuration } from '@/config'
+import { animation, animationDuration } from '@/config'
 
 const defaultAnimationOptions = {
   duration: animationDuration,
@@ -11,13 +11,17 @@ const animateToFrame = (
   frame: Rectangle,
   { duration, ease }: Partial<KeyframeOptions<Rectangle>> = defaultAnimationOptions,
 ) => {
-  animate({
-    from: window.frame(),
-    to: frame,
-    duration: (duration || defaultAnimationOptions.duration) * 1000,
-    ease: ease || defaultAnimationOptions.ease,
-    onUpdate: (newFrame: Rectangle) => window.setFrame(newFrame),
-  })
+  if (animation) {
+    animate({
+      from: window.frame(),
+      to: frame,
+      duration: (duration || defaultAnimationOptions.duration) * 1000,
+      ease: ease || defaultAnimationOptions.ease,
+      onUpdate: (newFrame: Rectangle) => window.setFrame(newFrame),
+    })
+  } else {
+    window.setFrame(frame)
+  }
 }
 
 export { animateToFrame }
