609a50591f33f1902c7daff9af3a2781fa890e7b
Marcos Moura <marcosvmmoura@gmail.com>
2022-08-11 02:56:49 +0200

    simplify git log command

--- dotfiles/.zsh/modules/git/scripts/utils.zsh
+++ dotfiles/.zsh/modules/git/scripts/utils.zsh
@@ -4,15 +4,13 @@ source ~/.zsh/utils.sh
 
 LOG_SEPARATOR="^"
 HASH="%C(yellow bold)%h"
-AUTHOR="%<(19,trunc)%C(green bold)%aN"
-DATE="%>|(45,trunc)%C(blue bold)%ad"
-MESSAGE="%<|(130,trunc)%C(reset)%s"
+AUTHOR="%C(green bold)%aN"
+DATE="%>>(14,trunc)%C(blue bold)%ad"
+MESSAGE="%<(80,trunc)%C(reset)%s"
 FORMAT=$(join_by_char $LOG_SEPARATOR $HASH $AUTHOR $DATE $MESSAGE)
 
 function format_log() {
-  TEMP_FILE=$(mktemp)
-  echo "$1" > $TEMP_FILE
-  FILE_TO_COLUMNS=`tsv-pretty -e -f -d=$LOG_SEPARATOR $TEMP_FILE`
-  bat -p --color=always -l="Markdown" --pager="less -RFnXJ" --theme="Sublime Snazzy" <<< $FILE_TO_COLUMNS
-  rm $TEMP_FILE
+  clear
+  PRETTY_PRINT=$(tsv-pretty -e -f -d=$LOG_SEPARATOR <<< $1)
+  bat -p --color=always -l="Markdown" --pager="less -RFnXJ" --theme="Sublime Snazzy" <<< $PRETTY_PRINT
 }
