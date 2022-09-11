4220d311c36d4ed7fa488db57c1703de122bbe62
Marcos Moura <marcosvmmoura@gmail.com>
2022-09-11 18:14:31 +0200

    fix delete-merged branches script

--- dotfiles/.zsh/modules/git/scripts/delete-merged.zsh
+++ dotfiles/.zsh/modules/git/scripts/delete-merged.zsh
@@ -1,23 +1,9 @@
 #!/usr/bin/env zsh
 
 source ~/.zsh/utils.sh
-source ~/.zsh/modules/git/scripts/utils.zsh
-source ~/.zsh/modules/git/git.plugin.zsh
 
 print_start "Checking for branches that can be deleted \n"
 
-git fetch -p
-git fetch upstream -p
-
-BRANCHES=`git branch -vv | awk "/: gone]/{print $1}"`
-
-if [ -z "${BRANCHES}" ]; then
-  print_info "No branches found"
-  return 1
-fi
-
-print_progress "Deleting all merged branches \n"
-
-$BRANCHES | xargs -n 1 git branch -D
+git-delete-merged-branches
 
 print_success "Done!"
