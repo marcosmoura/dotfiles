35b029cd937fb5a59f173884f6b7e1c083e15558
Marcos Moura <marcosvmmoura@gmail.com>
2022-11-11 19:29:44 +0100

    add missing dependencies

--- scripts/node.sh
+++ scripts/node.sh
@@ -7,9 +7,9 @@ fi
 
 print_progress "Installing yarn global packages"
 
-yarn global add @vue/cli
 yarn global add eslint
 yarn global add gtop
+yarn global add nx
 yarn global add prettier
 yarn global add typescript
 yarn global add vtop
