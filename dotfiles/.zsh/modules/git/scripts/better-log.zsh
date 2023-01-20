2a3874074e1ce519ab0eb969c0314814f3a84c74
Marcos Moura <marcosvmmoura@gmail.com>
2023-01-20 17:02:00 +0100

    add git autocomplete for aliases

--- dotfiles/.zsh/modules/git/scripts/better-log.zsh
+++ dotfiles/.zsh/modules/git/scripts/better-log.zsh
@@ -4,11 +4,5 @@ source ~/.zsh/utils.sh
 source ~/.zsh/modules/git/scripts/utils.zsh
 source ~/.zsh/modules/git/git.plugin.zsh
 
-LIMIT=$1
-
-if [ -z "${LIMIT}" ]; then
-  LIMIT=25
-fi
-
-LOG_OUTPUT=`git log -n $LIMIT --abbrev-commit --color --pretty=format:"$FORMAT"`
+LOG_OUTPUT=`git log -n 25 --abbrev-commit --color --pretty=format:"$FORMAT" $@`
 format_log $LOG_OUTPUT
