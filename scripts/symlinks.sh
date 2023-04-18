928e1602edd4be116d8ed572167ac8a50d85f005
Marcos Moura <marcosvmmoura@gmail.com>
2023-04-18 20:23:33 +0200

    fix install/update commands

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -14,11 +14,17 @@ for file in *; do
   print_purple "📁 Linked $file"
 done
 
-# Override Kitty config
+# Make Config folder
 HOME_CONFIG="$HOME/.config"
 mkdir -p "$HOME_CONFIG"
+
+# Override Kitty config
 rm -rf "$HOME_CONFIG/kitty"
 mv "$HOME/kitty" "$HOME_CONFIG/kitty"
+
+# Override Macchina config
+rm -rf "$HOME_CONFIG/macchina"
+mv "$HOME/macchina" "$HOME_CONFIG/macchina"
 
 shopt -u dotglob
 
