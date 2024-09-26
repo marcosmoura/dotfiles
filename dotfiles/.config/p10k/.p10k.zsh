3534df5651dab49727b5eaabd7c02e8395765943
Marcos Moura <marcosvmmoura@gmail.com>
2024-09-26 19:13:46 +0200

    prevent branch name from being cropped

--- dotfiles/.config/p10k/.p10k.zsh
+++ dotfiles/.config/p10k/.p10k.zsh
@@ -273,10 +273,7 @@
 
 		if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
 			local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
-			# If local branch name is at most 32 characters long, show it in full.
-			# Otherwise show the first 12 … the last 12.
-			# Tip: To always show local branch name in full without truncation, delete the next line.
-			(( $#branch > 32 )) && branch[13,-13]="…"  # <-- this line
+
 			res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}%B${branch//\%/%%}%b"
 		fi
 
