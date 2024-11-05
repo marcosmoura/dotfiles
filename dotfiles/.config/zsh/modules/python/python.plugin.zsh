a039165be2b20ad79d353b4f564ae63ab6d0e958
Marcos Moura <marcosvmmoura@gmail.com>
2024-11-05 18:07:58 +0100

    add topgrade for a much simpler system upgrade routine

--- dotfiles/.config/zsh/modules/python/python.plugin.zsh
+++ dotfiles/.config/zsh/modules/python/python.plugin.zsh
@@ -5,11 +5,3 @@ source ~/.config/zsh/utils.sh
 # Fix python3
 alias python=$(which python3)
 alias pip=$(which pip3)
-
-function updatePython {
-  print_start "Updating Python \n"
-
-  python -m pip list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 python -m pip install -U
-
-  print_success "Python updated! \n"
-}
