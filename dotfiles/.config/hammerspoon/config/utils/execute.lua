ff2def6b00b09f7561321a1e7010c8c1318284ac
Marcos Moura <marcosvmmoura@gmail.com>
2024-11-13 14:48:21 +0100

    refactor code for readability

--- dotfiles/.config/hammerspoon/config/utils/execute.lua
+++ dotfiles/.config/hammerspoon/config/utils/execute.lua
@@ -1,6 +1,5 @@
-local function execute(commandPath, args, opts)
+local getArgsAsString = function(args)
   local arguments = args or ""
-  local options = opts or {}
 
   if type(args) == "string" then
     arguments = args
@@ -10,27 +9,36 @@ local function execute(commandPath, args, opts)
     arguments = table.concat(args, " ")
   end
 
+  return arguments
+end
+
+local parseOutput = function(output, toJson)
+  if toJson and output ~= "" then
+    return hs.json.decode(output or {}) or {}
+  end
+
+  return output
+end
+
+local function execute(commandPath, args, opts)
+  local arguments = getArgsAsString(args)
+  local options = opts or {}
+
   local command = commandPath .. " " .. arguments
-  local output, status = hs.execute(command)
+  local stdOut, status = hs.execute(command)
+  local output = parseOutput(stdOut, options.json)
 
   if not status then
     if not options.silent then
-      print("Error executing command: " .. command)
-      print(output)
+      print("Error executing command:")
+      print(command)
+      Print(stdOut)
     end
 
-    if options.json then
-      return {}, status
-    end
-
-    return "", status
+    return output, false
   end
 
-  if options.json then
-    return hs.json.decode(output or {}) or {}, status
-  end
-
-  return output or "", status
+  return output, true
 end
 
 return execute
