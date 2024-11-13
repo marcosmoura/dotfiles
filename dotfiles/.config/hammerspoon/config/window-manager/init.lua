33f7c31b601498bdb297e1cb3e1540f4335c53e0
Marcos Moura <marcosvmmoura@gmail.com>
2024-11-13 14:42:21 +0100

    make sure yabai is running before starting the service

--- dotfiles/.config/hammerspoon/config/window-manager/init.lua
+++ dotfiles/.config/hammerspoon/config/window-manager/init.lua
@@ -12,6 +12,14 @@ local hasBordersService = function()
   return bordersService and bordersService[1] and bordersService[1].running
 end
 
+local isYabaiRunning = function()
+  local services = execute("launchctl list")
+  local normalizedServices = type(services) == "table" and table.concat(services, "") or services
+  local matchesYabai = string.find(normalizedServices, "yabai")
+
+  return matchesYabai ~= nil and matchesYabai ~= ""
+end
+
 module.start = function(withYabai)
   local hasBordersEnabled = hasBordersService()
 
@@ -19,19 +27,23 @@ module.start = function(withYabai)
   require("config.window-manager.bindings").start()
 
   if withYabai then
-    require("config.window-manager.stack-indicator").start() -- New module
-    executeYabai("--start-service", { silent = true })
+    if not isYabaiRunning() then
+      executeYabai("--start-service", { silent = true })
+    end
 
     if not hasBordersEnabled then
       execute("/opt/homebrew/bin/brew", "services start borders", { silent = true })
     end
+
+    require("config.window-manager.stack-indicator").start() -- New module
   else
+    executeYabai("--stop-service", { silent = true })
+
     if not hasBordersEnabled then
       require("config.window-manager.experimental.borders").start() -- New module
     end
 
     require("config.window-manager.experimental.tiling").start() -- New module
-    executeYabai("--stop-service", { silent = true })
   end
 end
 
