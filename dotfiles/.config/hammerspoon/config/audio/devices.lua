271bc15333a0a5e7d7304998c84080a96bf6c4b4
Marcos Moura <marcosvmmoura@gmail.com>
2024-11-14 15:24:44 +0100

    move audio config to a dedicated folder

--- dotfiles/.config/hammerspoon/config/audio/devices.lua
+++ dotfiles/.config/hammerspoon/config/audio/devices.lua
@@ -22,14 +22,12 @@ local findDeviceByName = memoize(function(devices, name)
   end)
 end)
 
-local getTargetOutputDevice = function()
-  local outputDevices = hs.audiodevice.allOutputDevices()
-
-  local airpods = findDeviceByName(outputDevices, "airpods")
-  local teamsAudio = findDeviceByName(outputDevices, "microsoft teams audio")
-  local speakers = findDeviceByName(outputDevices, "external speakers")
-  local audioInterface = findDeviceByName(outputDevices, "minifuse")
-  local macbookPro = findDeviceByName(outputDevices, "MacBook Pro")
+local getTargetOutputDevice = memoize(function(devices)
+  local airpods = findDeviceByName(devices, "airpods")
+  local teamsAudio = findDeviceByName(devices, "microsoft teams audio")
+  local speakers = findDeviceByName(devices, "external speakers")
+  local audioInterface = findDeviceByName(devices, "minifuse")
+  local macbookPro = findDeviceByName(devices, "MacBook Pro")
 
   if airpods then
     return airpods
@@ -48,15 +46,40 @@ local getTargetOutputDevice = function()
   end
 
   return nil
+end)
+
+local normalizeBalance = function(balance)
+  return string.format("%.4f", balance)
 end
+
+local setSpeakersBalance = memoize(function(devices)
+  local speakers = findDeviceByName(devices, "external speakers")
+
+  if not speakers then
+    return
+  end
+
+  local targetAudioBalance = 0.4625
+
+  if normalizeBalance(speakers:balance()) == normalizeBalance(targetAudioBalance) then
+    return
+  end
+
+  print(string.format("Setting %s balance to %s", speakers:name(), targetAudioBalance))
+
+  speakers:setBalance(targetAudioBalance)
+end)
 
 local handleOutputDeviceChange = function()
   local current = hs.audiodevice.defaultOutputDevice()
-  local targetDevice = getTargetOutputDevice() or current
+  local outputDevices = hs.audiodevice.allOutputDevices()
+  local targetDevice = getTargetOutputDevice(outputDevices) or current
 
   if not targetDevice or current and current:name() == targetDevice:name() then
     return
   end
+
+  setSpeakersBalance(outputDevices)
 
   targetDevice:setDefaultOutputDevice()
   targetDevice:setDefaultEffectDevice()
@@ -80,19 +103,21 @@ local handleInputDeviceChange = function()
   print("Default input device set to " .. targetDevice:name())
 end
 
-local onAudioChange = function()
+local onAudioDeviceChange = function()
   handleOutputDeviceChange()
   handleInputDeviceChange()
 end
 
 module.start = function()
-  hs.caffeinate.watcher.new(onAudioChange):start()
-  hs.usb.watcher.new(onAudioChange):start()
-  hs.screen.watcher.newWithActiveScreen(onAudioChange):start()
-  hs.audiodevice.watcher.setCallback(onAudioChange)
+  hs.caffeinate.watcher.new(onAudioDeviceChange):start()
+  hs.usb.watcher.new(onAudioDeviceChange):start()
+  hs.screen.watcher.newWithActiveScreen(onAudioDeviceChange):start()
+  hs.audiodevice.watcher.setCallback(function()
+    onAudioDeviceChange()
+  end)
   hs.audiodevice.watcher.start()
 
-  onAudioChange()
+  onAudioDeviceChange()
 end
 
 return module
