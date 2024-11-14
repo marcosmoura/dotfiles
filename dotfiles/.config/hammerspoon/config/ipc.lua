0670fdd8a1f9945687f7440d85b86e4ed9d5b6d0
Marcos Moura <marcosvmmoura@gmail.com>
2024-11-14 15:21:50 +0100

    move ipc logic to a dedicated file

---
+++ dotfiles/.config/hammerspoon/config/ipc.lua
@@ -0,0 +1,12 @@
+local module = {}
+
+module.start = function()
+  hs.ipc.cliSaveHistory(true)
+  hs.ipc.cliSaveHistorySize(99999)
+
+  if not hs.ipc.cliStatus() then
+    hs.ipc.cliInstall()
+  end
+end
+
+return module
