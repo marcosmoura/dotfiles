6db6e235b4840029779fe3dde3136ac1e449faf9
Marcos Moura <marcosvmmoura@gmail.com>
2024-11-15 16:02:11 +0100

    remove unused spoons

--- dotfiles/.config/hammerspoon/config/spoons.lua
+++ dotfiles/.config/hammerspoon/config/spoons.lua
@@ -7,32 +7,6 @@ module.start = function()
 
   spoon.SpoonInstall.use_syncinstall = true
   spoon.SpoonInstall:andUse("EmmyLua")
-  spoon.SpoonInstall:andUse("MiddleClickDragScroll", {
-    start = true,
-    config = {
-      indicatorSize = 24,
-      indicatorAttributes = {
-        type = "circle",
-        strokeColor = { alpha = 0 },
-        fillColor = { hex = colors.surface2.hex, alpha = 0.5 },
-      },
-    },
-  })
-  spoon.SpoonInstall:andUse("PopupTranslateSelection", {
-    config = {
-      popup_style = hs.webview.windowMasks.utility
-        | hs.webview.windowMasks.HUD
-        | hs.webview.windowMasks.titled
-        | hs.webview.windowMasks.closable
-        | hs.webview.windowMasks.resizable,
-    },
-    hotkeys = {
-      translate_to_en = { { "cmd", "alt", "ctrl" }, "t" },
-    },
-  })
-  spoon.SpoonInstall:andUse("AllBrightness", {
-    start = true,
-  })
   spoon.SpoonInstall:andUse("ReloadConfiguration", {
     start = true,
   })
