4f5d135fa68325fb7ad25b15414556b53f95cacb
Marcos Moura <marcosvmmoura@gmail.com>
2025-01-30 01:16:55 +0100

    fix rust installation script

--- installation/rust.sh
+++ installation/rust.sh
@@ -1,9 +1,7 @@
 print_start "Installing Rust"
 
 if ! brew ls --versions rust >/dev/null; then
-  brew install rust
-  brew install rustup-init
-  rustup toolchain install stable
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 print_progress "Installing Rust packages"
