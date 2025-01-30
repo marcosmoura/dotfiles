5686824f5bca077f2ba9c76323e89728aeb2459e
Marcos Moura <marcosvmmoura@gmail.com>
2025-01-30 01:07:07 +0100

    remove nvim backup config

--- dotfiles/.config/nvim_old/lua/config/plugins/editor/sessions.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = {
-      need = 0,
-    },
-  },
-}
--- dotfiles/.config/nvim_old/lua/config/settings/init.lua
+++
@@ -1,3 +0,0 @@
-require("config.settings.options")
-require("config.settings.mappings")
-require("config.settings.autocmd")
