bf89eac98fee705f5e24f0af3f581247464e5740
Marcos Moura <marcosvmmoura@gmail.com>
2025-01-30 01:12:48 +0100

    add cspell configuration

--- installation/node.sh
+++ installation/node.sh
@@ -7,16 +7,53 @@ if [[ $? != 0 ]]; then
   brew install bun
 fi
 
-print_progress "Installing yarn global packages"
+print_progress "Installing global packages"
 
-pnpm i -g @fsouza/prettierd
-pnpm i -g @johnnymorganz/stylua-bin
-pnpm i -g @vscode/codicons
-pnpm i -g eslint
-pnpm i -g eslint_d
-pnpm i -g nx
-pnpm i -g prettier
-pnpm i -g stylelint
-pnpm i -g typescript
+global_packages=(
+  "@fsouza/prettierd"
+  "@johnnymorganz/stylua-bin"
+  "@vscode/codicons"
+  "eslint_d"
+  "eslint"
+  "nx"
+  "prettier"
+  "stylelint"
+  "typescript"
+)
+
+for package in "${global_packages[@]}"; do
+  pnpm i -g "$package"
+done
+
+print_progress "Installing cspell"
+
+cspell_dictionaries=(
+  "@cspell/dict-companies"
+  "@cspell/dict-css"
+  "@cspell/dict-en-common-misspellings"
+  "@cspell/dict-filetypes"
+  "@cspell/dict-fonts"
+  "@cspell/dict-html"
+  "@cspell/dict-html-symbol-entities"
+  "@cspell/dict-lua"
+  "@cspell/dict-markdown"
+  "@cspell/dict-node"
+  "@cspell/dict-npm"
+  "@cspell/dict-people-names"
+  "@cspell/dict-pt-br"
+  "@cspell/dict-public-licenses"
+  "@cspell/dict-rust"
+  "@cspell/dict-shell"
+  "@cspell/dict-software-terms"
+  "@cspell/dict-typescript"
+  "@cspell/dict-vim"
+)
+
+pnpm i -g cspell
+
+for dictionary in "${cspell_dictionaries[@]}"; do
+  pnpm i -g "$dictionary"
+  cspell link add "$dictionary"
+done
 
 print_success "Node installed! \n"
