b53253057abfc23d7d2cbed83064ef6e5b4b5999
Marcos Moura <marcosvmmoura@gmail.com>
2025-02-06 01:19:30 +0100

    fix rainbow delimiters

--- home/.config/nvim/lua/plugins/editor/editor.lua
+++ home/.config/nvim/lua/plugins/editor/editor.lua
@@ -7,7 +7,16 @@ return {
     "HiPhish/rainbow-delimiters.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require("rainbow-delimiters.setup").setup({})
+      require("rainbow-delimiters.setup").setup({
+        query = {
+          -- Use parentheses by default
+          [""] = "rainbow-delimiters",
+          tsx = "rainbow-parens",
+          typescript = "rainbow-parens",
+          javascript = "rainbow-parens",
+          html = "rainbow-parens",
+        },
+      })
     end,
   },
 }
