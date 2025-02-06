84b7650a38f64998ae63a2156f81ba98b5179ab5
Marcos Moura <marcosvmmoura@gmail.com>
2025-02-06 01:25:40 +0100

    refactor emoji function

--- home/.config/starship/clock.sh
+++ home/.config/starship/clock.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 # print a emoji representing the current clock hour
-echo -n "" | cut -c $(($(date +%I) + 1))
+hour=$(date +"%H")
+case $hour in
+00 | 12) echo "" ;;
+01 | 13) echo "" ;;
+02 | 14) echo "" ;;
+03 | 15) echo "" ;;
+04 | 16) echo "" ;;
+05 | 17) echo "" ;;
+06 | 18) echo "" ;;
+07 | 19) echo "" ;;
+08 | 20) echo "" ;;
+09 | 21) echo "" ;;
+10 | 22) echo "" ;;
+11 | 23) echo "" ;;
+esac
