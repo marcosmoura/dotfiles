0594048a81ebe8f696de9767f6fc38df6c255712
Marcos Moura <marcosvmmoura@gmail.com>
2025-02-11 13:13:10 +0100

    use mini.animate instead of snacks animate

--- home/.config/nvim/lua/plugins/ui/snacks.lua
+++ home/.config/nvim/lua/plugins/ui/snacks.lua
@@ -2,10 +2,6 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      animate = {
-        enabled = true,
-        fps = 240,
-      },
       notifier = {
         enabled = true,
         timeout = 6000,
