301ecfcdb6a7031d5ef552eba2cd6fd346cb1ce5
Marcos Moura <marcosvmmoura@gmail.com>
2025-02-11 13:08:42 +0100

    enable animation

--- home/.config/nvim/lua/plugins/ui/snacks.lua
+++ home/.config/nvim/lua/plugins/ui/snacks.lua
@@ -3,9 +3,11 @@ return {
     "folke/snacks.nvim",
     opts = {
       animate = {
+        enabled = true,
         fps = 240,
       },
       notifier = {
+        enabled = true,
         timeout = 6000,
         top_down = false,
       },
