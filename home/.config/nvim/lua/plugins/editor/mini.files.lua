9125a11c0df0c5d690e29ee009863bb373e17951
Marcos Moura <marcosvmmoura@gmail.com>
2025-02-11 13:03:14 +0100

    enable mini-files together with snacks picker

--- home/.config/nvim/lua/plugins/editor/mini.files.lua
+++ home/.config/nvim/lua/plugins/editor/mini.files.lua
@@ -1,7 +1,6 @@
 return {
   {
     "echasnovski/mini.files",
-    enabled = false,
     opts = {
       windows = {
         width_focus = 35,
@@ -24,13 +23,50 @@ return {
         end,
         desc = "Open mini.files (Directory of Current File)",
       },
-      {
-        "<leader>e",
-        function()
-          require("mini.files").open(vim.uv.cwd(), true)
-        end,
-        desc = "Open mini.files (cwd)",
-      },
     },
+    init = function()
+      local function augroup(name)
+        return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
+      end
+
+      --- Perform an action when the last buffer is deleted
+      --- @param callback function
+      local function on_last_buffer_deleted(callback)
+        return function(event)
+          local name = vim.api.nvim_buf_get_name(event.buf or 0)
+          local filetype = vim.api.nvim_get_option_value("filetype", { buf = event.buf or 0 })
+          local show_dashboard = not name and not filetype
+
+          if show_dashboard then
+            callback()
+          end
+        end
+      end
+
+      local function close_minifiles()
+        require("mini.files").close()
+      end
+
+      -- Open dashboard after last buffer is deleted
+      vim.api.nvim_create_autocmd("User", {
+        group = augroup("snacks_dashboard"),
+        pattern = "BDeletePost",
+        callback = on_last_buffer_deleted(function()
+          close_minifiles()
+          require("snacks").dashboard.open()
+          require("snacks").dashboard.update()
+        end),
+      })
+
+      -- Reload dashboard on session load
+      vim.api.nvim_create_autocmd("User", {
+        group = augroup("snacks_dashboard"),
+        pattern = "PersistenceLoadPost",
+        callback = on_last_buffer_deleted(function()
+          close_minifiles()
+          require("snacks").dashboard.update()
+        end),
+      })
+    end,
   },
 }
