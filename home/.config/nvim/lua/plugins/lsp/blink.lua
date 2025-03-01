fcd0fe40328a23f2f94129bbd322155dacf22ac3
Marcos Moura <marcosvmmoura@gmail.com>
2025-03-01 23:45:27 +0100

    feat(neovim): force blink as default complete engine

---
+++ home/.config/nvim/lua/plugins/lsp/blink.lua
@@ -0,0 +1,13 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = false,
+        },
+      },
+    },
+  },
+}
