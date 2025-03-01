06fee5fa3d10834932ba459df26b35fb82b9ed32
Marcos Moura <marcosvmmoura@gmail.com>
2025-03-01 23:06:50 +0100

    fix(neovim): make eslint recognize correct dependency paths

--- home/.config/nvim/lua/plugins/linting/linting.lua
+++ home/.config/nvim/lua/plugins/linting/linting.lua
@@ -117,4 +117,17 @@ return {
       })
     end,
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, {
+        eslint = {
+          settings = {
+            workingDirectory = { mode = "location" },
+          },
+        },
+      })
+    end,
+  },
 }
